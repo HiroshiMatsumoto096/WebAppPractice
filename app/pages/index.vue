74a21bbd4eeff62a979fe30859bbb2de0f59ba49
matsumoto <h_matsumoto@hotmail.com>
2023-08-28 01:13:20 +0900

    add vuetify

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// index.vue
+</script>
+
+<template>
+    <v-card variant="outlined" class="ma-6">
+        <v-card-title align="center">
+            TODO: 掃除 
+        </v-card-title>
+        <v-card-text align="center">
+            ゴミ出し 
+        </v-card-text>
+    </v-card>
+</template>
+
+
