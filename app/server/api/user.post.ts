58fdeaca50457dd581bcd112f390fa1559c5d615
HiroshiMatsumoto <matsumoto@gmail.com>
2023-09-08 09:09:36 +0900

    ユーザのcreateMany

--- app/server/api/user.post.ts
+++ app/server/api/user.post.ts
@@ -5,6 +5,21 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     console.log("uesr.posts.ts.start")
     console.log(body)
+    if(body instanceof(Array)){ // 配列入力の場合
+        console.log("array が入力されました")
+        const user = await prisma.user.createMany({
+            data: body,
+        })
+    } else { // オブジェクト入力の場合
+        console.log("object が入力されました")
+        const user = await prisma.user.create({
+            data: {
+                name: body.name,
+                email: body.email
+            },
+        })
+    }
+    /*
     const user = await prisma.user.create({
         data: {
             name: body.name,
@@ -13,6 +28,7 @@ export default defineEventHandler(async (event) => {
     })
     const response = await prisma.$disconnect()
     console.log(response)
+    */
     console.log("uesr.posts.ts.end")
 })
 
