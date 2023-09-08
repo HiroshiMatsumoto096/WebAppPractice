522a1c7a12a7596f7325292f77b00042e2cc8741
HiroshiMatsumoto <matsumoto@gmail.com>
2023-09-08 09:14:54 +0900

    ユーザのfindMany, findUnique対応

--- app/server/api/user.ts
+++ app/server/api/user.ts
@@ -4,5 +4,24 @@ const prisma = new PrismaClient()
 export default defineEventHandler(async (event) => { 
     console.log('index.ts')
     const users = await prisma.user.findMany()
-    return users
+    // const query = getQuery(event)
+    // console.log(query)
+    console.log(prisma)
+    /*
+    let return_value
+    if(! query.id){
+        console.log("id not specified")
+        return_value = await prisma.user.findMany()
+    } else {
+        console.log("id specified")
+        console.log(query)
+        return_value = prisma.user.findUnique({
+            where: {
+                id: parseInt(query.id)
+            }
+        })
+    }
+    return return_value
+    */
+   // return prisma
 })
